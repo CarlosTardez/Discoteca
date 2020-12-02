2ea8199eee547fef015c73373fea75a50f93877b
CarlosTardez <71508391+CarlosTardez@users.noreply.github.com>
2020-12-02 20:29:48 +0100

    Update UsuarioUtils.java
    Función para comprobar validez de las contraseñas

--- src/main/java/DI2021/Discoteca/utils/UsuarioUtils.java
+++ src/main/java/DI2021/Discoteca/utils/UsuarioUtils.java
@@ -1,8 +1,25 @@
 package DI2021.Discoteca.utils;
 
-public class UsuarioUtils {
+public class UsuarioUtils extends Exception {
+
+	// Funcion validar calidad password
 
 	public static boolean ValidarCalidadPassword(String password) {
-		return (Boolean) null;
+		char[] valor = password.toCharArray();
+		boolean numero = false;
+
+		for (char a : valor) {
+			if (Character.isDigit(a))
+				numero = true;
+		}
+
+		if (numero == false)
+			throw new IllegalArgumentException("Contraseña incorrecta. Debe introducir al menos un número");
+
+		if (password.length() < 9)
+			throw new IllegalArgumentException(
+					"Contraseña incorrecta. La contraseña debe contener al menos 8 carácteres entre ellos un número ");
+
+		return true;
 	}
 }
