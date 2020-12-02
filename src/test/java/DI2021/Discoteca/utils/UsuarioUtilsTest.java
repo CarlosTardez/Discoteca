36ed336f9ac7d2e9219f8268f0ba7531f696f209
CarlosTardez <71508391+CarlosTardez@users.noreply.github.com>
2020-12-02 20:32:20 +0100

    Update UsuarioUtilsTest.java
    Creación de dos tests que comprueban la función de validez de contraseñas

--- src/test/java/DI2021/Discoteca/utils/UsuarioUtilsTest.java
+++ src/test/java/DI2021/Discoteca/utils/UsuarioUtilsTest.java
@@ -12,10 +12,32 @@ import org.junit.Test;
  *
  */
 public class UsuarioUtilsTest {
-
+	
+	//Test que valida contraseña correcta
 	@Test
-	public void ValidarCalidadPassword() {
-		fail("Not yet implemented");
+	public void ValidarCalidadPasswordCorrecta() {
+		String password = "carlos123";
+		
+		boolean resultadoEsperado = true;
+		boolean resultadoReal = UsuarioUtils.ValidarCalidadPassword(password);
+		assertEquals(resultadoEsperado, resultadoReal);
 	}
-
+	
+	//Test que valida contraseña incorrecta
+	@Test
+	public void ValidarCalidadPasswordIncorrecta() {
+		String password = "carlos";
+		String password2 = "carlooooos";
+		
+		boolean resultadoEsperado = false;
+		
+		//test de comprobacion de contraseña con string sin 8 caracteres
+		boolean resultadoReal = UsuarioUtils.ValidarCalidadPassword(password);
+		//test de comprobacion de contraseña con string sin número
+		boolean resultadoReal2 = UsuarioUtils.ValidarCalidadPassword(password2);
+		
+		assertEquals(resultadoEsperado, resultadoReal);
+		assertEquals(resultadoEsperado, resultadoReal2);
+	}
 }
+
